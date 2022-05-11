a6e4fbc3d441406f8323cad78eddc1e724ee68a6
Nestor Lenaerts <nestor.lenaerts@gmail.com>
2022-05-11 22:52:24 +0200

    updated

---
+++ H6_Gevorderde_Overervingsconcepten/H6_Geometric Figures Class/GeometricFigure.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace H6_Gevorderde_Overervingsconcepten.H6_Geometric_Figures_Class
+{
+    public abstract class GeometricFigure
+    {
+        public int Hoogte { get; set; }
+        public int Breedte { get; set; }
+
+        public int Oppervlakte { get { return BerekenOppervlakte(); } }
+        public abstract int BerekenOppervlakte();
+
+
+
+    }
+    public class Rechthoek : GeometricFigure
+    {
+        
+        public override int BerekenOppervlakte()
+        {
+            return Breedte * Hoogte;
+        }
+
+    }
+    public class Vierkant : Rechthoek
+    {
+        public Vierkant(int HIGH, int THICK)
+        {
+            if (HIGH != THICK)
+            {
+                THICK = HIGH;
+                Breedte = THICK;
+                Hoogte = THICK;
+            }
+            else
+            {
+                Breedte = THICK;
+                Hoogte = HIGH;
+            }
+            
+        }
+        public Vierkant(int lengte)
+        {
+            Breedte = lengte;
+            Hoogte = lengte;
+        }
+    }
+
+    public class Driehoek : GeometricFigure
+    {
+        public override int BerekenOppervlakte()
+        {
+            return Breedte * Hoogte / 2;
+        }
+    }
+
+}
