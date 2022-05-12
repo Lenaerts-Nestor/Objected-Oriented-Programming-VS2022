14522707a91f0b649909d83543ec9c10229d63ff
Nestor Lenaerts <nestor.lenaerts@gmail.com>
2022-05-12 15:43:09 +0200

    d

--- H8_Polymorfisme/Een eigen huis/Huis.cs
+++ H8_Polymorfisme/Een eigen huis/Huis.cs
@@ -6,8 +6,51 @@ using System.Threading.Tasks;
 
 namespace H8_Polymorfisme.Een_eigen_huis
 {
-    class Huis
+    public class Huis
     {
+        Kamer[] MultipleKamers = new Kamer[3]; //3 kamers in totaal
+        public void BerekenPrijs()
+        {
+            int Totaal = 0;
 
+            for (int i = 0; i < MultipleKamers.Length; i++)
+            {
+                Totaal += MultipleKamers[i].Prijs;
+            }
+        }
+
+    }
+    public class Kamer
+    {
+        public virtual int Oppervlakte { get; set; } //vierkante meter
+        public virtual int Prijs { get { return 400; } }//400 euro's
+        public bool schouw { get; set; }
+
+    }
+    public class BadKamer : Kamer
+    {
+        public override int Prijs { get { return 500; } }   
+    }
+    public class Gang: Kamer
+    {
+        public override int Prijs { get { return Oppervlakte * 10; } }
+    }
+    public class Salon : Kamer
+    {
+        public override int Prijs
+        {
+            get
+            {
+                if (!schouw)
+                {
+                    return 500;
+                }
+                else
+                {
+                    return 300;
+                }
+
+            }
+        }
     }
 }
--- H8_Polymorfisme/Program.cs
+++ H8_Polymorfisme/Program.cs
@@ -7,6 +7,11 @@ namespace H8_Polymorfisme
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            ///Gebruik compositie én overerving om een klasse Huis te voorzien van enkele kamers,
+            ///waarbij iedere kamer een klasse op zich is (denk aan BadKamer, Salon, etc). 
+            ///Alle kamers erven over van de klasse Kamer. Iedere kamer heeft een oppervlakte (in vierkante meter),
+            ///een naam en prijs. Standaard is de prijs van een kamer 400euro, maar mogelijke child-klassen zullen deze property soms overriden. 
+            ///De Prijs is een readonly property (zonder setter, en heeft ook geen achterliggende instantievariabele
         }
     }
 }
